4926a1bec4b6064743902c6ba25693bc16c816a6
Rayyan <syedrayyaann00@gmail.com>
2025-11-06 23:37:08 +0530

    Add imports for request handling and model prediction

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import joblib
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+# @app.route('/predict', methods=['POST'])
+# def predict():
+#     data = request.get_json()
+#     model = joblib.load('model.joblib')
+#     prediction = model.predict([data['features']])
+#     return jsonify({'prediction': prediction.tolist()})
 
 if __name__ == '__main__':
     app.run(debug=True)
